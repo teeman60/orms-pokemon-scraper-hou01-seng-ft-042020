168f5702ead613fbce4c4285759356371327329c
Taiye Salami <taiye41@gmail.com>
2020-05-04 03:30:25 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,6 +13,7 @@ class Pokemon
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name type)
   end
+end
 
 #   def self.find(id, db)
 #     id = db.execute("SELECT id FROM pokemon WHERE id = ?", id).flatten[0]
