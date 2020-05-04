5a0fe4d1eab9532f6e647cbd4a92c8ab176c91fd
Taiye Salami <taiye41@gmail.com>
2020-05-04 03:11:02 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,24 +1,24 @@
-class Pokemon
-  attr_accessor :db, :name, :type, :id 
+# class Pokemon
+#   attr_accessor :db, :name, :type, :id 
   
-  def initialize(id: id, name: name, type: type, db: db)
-    @id = id
-    @name = name
-    @type = type
-    @db = db
-  end
+#   def initialize(id: id, name: name, type: type, db: db)
+#     @id = id
+#     @name = name
+#     @type = type
+#     @db = db
+#   end
   
   
-  def self.save(name, type, db)
-    db.execute("INSERT INTO Pokemon (name, type) VALUES (?, ?)", name type)
-  end
+#   def self.save(name, type, db)
+#     db.execute("INSERT INTO Pokemon (name, type) VALUES (?, ?)", name type)
+#   end
   
-  def self.find(id, db)
-    id = db.execute("SELECT id FROM pokemon WHERE id = ?", id).flatten[0]
-    name = db.execute("SELECT name FROM pokemon WHERE id = ?", id).flatten[0]
-    type = db.execute("SELECT type FROM pokemon WHERE id = ?", id).flatten[0]
+#   def self.find(id, db)
+#     id = db.execute("SELECT id FROM pokemon WHERE id = ?", id).flatten[0]
+#     name = db.execute("SELECT name FROM pokemon WHERE id = ?", id).flatten[0]
+#     type = db.execute("SELECT type FROM pokemon WHERE id = ?", id).flatten[0]
     
-    Pokemon.new(id: id, name: name, type: type, db: db)
-  end
+#     Pokemon.new(id: id, name: name, type: type, db: db)
+#   end
   
-end
+# end
